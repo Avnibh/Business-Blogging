7479146d52879314d522a1d3bf73480e733c7323
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-24 00:04:31 +0530

    Add files via upload

---
+++ business-businessdevelopment-workspace.php
@@ -0,0 +1,255 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>WORKSPACE</h3>
+						<p>Today’s workplaces constitute of different types of applications, various types of devices, and employees, who supposed to have every possible information at any time, at hand and anywhere. Across the globe, various workspace needs to evolve to a connecting application, information, data, workspace aggregator, identities and services among others. In addition, to provide a seamless operation among the enterprise’s workspace aggregator has an important role. This workspace aggregator is a type of software or client application or a web portal that mutually able to provide multirole applications which includes virtual desktop infrastructure, Dropbox and Skype with data and devices to deliver a single access point. The consumer can able to do their work in all or some of workspace components such as mobile apps, virtual apps, software as a service (SaaS) apps, physical apps, Dropbox account, Box account, AD account, Twitter account, Skype account, Google account, virtual desktop and Live ID among others from their corporate or own devices such as laptops tablets, PCs and smartphones among others. Moreover, increasing the demand of mobile workforce has ensued in the early adoption of workspace aggregator solutions among enterprises globally. Furthermore, increasing adoption of this technology and better operation capabilities by various end use industry is boosting the demand of global Workspace Aggregator market during the forecast period. The market of Workspace Aggregator is predicted a stable during the forecast period from 2017 to 2025.</p>
+						<p>Geographically, the global workspace aggregator market has been segmented into North America, Europe, Asia Pacific, Middle East & Africa, and Latin America. In 2016, Asia Pacific and North America were major contributors to the global workspace aggregator market, accounting cumulatively for over 50% of the share owing to various organizations installing these workspace aggregators to increase productivity and maintain security. In addition, major workspace aggregator providers are focusing on expanding their business in the untapped Asia Pacific and Europe market. Furthermore, the growing demand for data management, mobile workforce, and digitalization in emerging economies which includes China, Singapore, India, Japan, and South Korea is aiding the growth of this market in the region.  </p>
+						<blockquote class="blockquote">
+							<p>Some of the leading players in the global workspace aggregator market are SAP SE (Germany), Citrix Systems (The U.S), VMware (The U.S), Microsoft Corporation (The U.S), ASG Software (The U.S), Symantec (the U.S), Intel Security (The U.S), Cisco Systems (The U.S), CA Technologies (The U.S), IBM (The U.S)</p>
+						</blockquote>
+						<p></p>
+					</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+				<!--	<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Social</a></li>
+								<li><a href="#">Lifestyle</a></li>
+								<li><a href="#">Fashion</a></li>
+								<li><a href="#">Health</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">NANDHAVARDHAN REDDY</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/nandhavardhan.jpeg" alt="nandhavardhan_fuerte">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="research-R&D.php"><img src="./img/BLOGS/R&D.jpg" alt="R&D"></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="research-R&D.php">Business Development</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="research-R&D.php">Research & Development</a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Avni Bhardwaj</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="social-google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+              <!-- /social widget -->
+
+					<!-- category widget -->
+					<?php
+					include "cate.php"
+					?>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<?php
+					include "instagram.php"
+					?>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
