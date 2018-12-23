591a1341673cd17eadf86d17db0f56460e8bc470
Avni Bhardwaj <40702176+Avnibh@users.noreply.github.com>
2018-12-24 00:04:47 +0530

    Add files via upload

---
+++ business-e-commerce.php
@@ -0,0 +1,291 @@
+<?php
+include "header.php";
+?>
+<body>
+	<!-- section -->
+	<div class="section">
+		<!-- container -->
+		<div class="container">
+			<!-- row -->
+			<div class="row">
+				<div class="col-md-8">
+					<!-- post share -->
+					<div class="section-row">
+						<div class="post-share">
+							<a href="#" class="social-facebook"><i class="fa fa-facebook"></i><span>Share</span></a>
+							<a href="#" class="social-twitter"><i class="fa fa-twitter"></i><span>Tweet</span></a>
+							<a href="#" class="social-pinterest"><i class="fa fa-pinterest"></i><span>Pin</span></a>
+							<a href="#" ><i class="fa fa-envelope"></i><span>Email</span></a>
+						</div>
+					</div>
+					<!-- /post share -->
+
+					<!-- post content -->
+					<div class="section-row">
+						<h3>E-COMMERCE ONLINE WEBSITES
+
+</h3>
+						<p>Now – a-days E-commerce online websites were become very common and with this websites people earning a crore of rupees. Now we will discuss about the things that are more essential to build a website.</p>
+						<p><h4>Ten things that every one should remember when building a website: </p></h4>
+
+                        <p>
+<br>1.	Start with your business name: In this section, we have to select a memorable and fabulous name which no one has already. You have to check whether the name is used or not in google. Once you select the name of the website you should register it.<br>
+
+<br>2.	Secure your domain name and website: Ideally, you’ll get the same domain name as your business name, if not then choose a simple URL which can spell easily by people. The design of e-commerce website is the biggest expense you have. But you ensure that it not only visually appealing, but also perfect in functioning. There are out-of-the-box eCommerce solutions like shop to begin with, but you may require something more custom-made if your needs are more than basic.<br>
+
+<br>3.	Select the business structure and register your business: you have got several options when it comes to business structure.              <br>                                                    
+<br>•	Sole proprietor<br>
+•	LLC<br>
+•	Partnership<br>
+•	Corporation<br>
+<br>if you don’t want to choose corporation and LLC, you immediately take sole proprietor or partnership. If you choose sole proprietor, your personal assets are at risk. If your company is ever sued, the court can seize your personal assets if your business doesn’t have enough to cover its debt. Both the corporation and LLC separate you and your assets from the business and provide tax benefits.<br>
+
+
+
+<br>4.	Get your Employer identification number:  you’ll need an Employer Identification Number (EIN) to open a business bank account and file your business taxes. Your EIN is just like a business security number: it’s a unique number that identifies your business and helps to file an important paperwork. Everyone needs one if you have employee or not. <br>
+
+<br>5.	Apply for business license and permits: operating such e-commerce business doesn’t exclude you from having certain license and permits. Check with in your city, state and country to see what type of sales tax licenses and home business license you need, get them approved before you get operated.<br>
+
+
+<br>6.	Find the right vendors: you’ll have a lot of competition in selling products so please keep it in mind that the materials you are used to make products is of best quality and the prices that you sell your products should be as minimum as possible.<br>
+
+<br>7.	Start marketing early: even though your website is not online please made a coming soon page to attract people and people who attracted will get registered as soon as possible.<br>  
+
+
+<br>8.	Get more productive with right software: Technology makes your work as simple as possible before you get online, operate with customer relationship management, email marketing software, accounting, project management and inform them what you will going to do once you get online.<br>
+
+<br>9.	Stock your inventory: Whether you’ve got a warehouse full of products somewhere or your inventory lives in your garage, make sure you’ve got enough to launch. It can be tricky, not knowing how much you’ll need, but in general, it’s better to have too much inventory than not enough. Pay attention to how your sales increase so you can be smart with future orders.<br>
+
+<br>10.	Make sure that your business stays compliant: 
+Once you launch your eCommerce business, things are going to move at the speed of light. Don’t overlook filing your annual report, if you incorporated or filed an LLC, nor those annual fees for business permits. If you need to, put these deadlines on your calendar so you stay on top of them.<br>
+</p>
+<figure class="pull-centre">
+							<img src="./img/BLOGS/e-commerce.jpg" alt="e-commerce">
+							<figcaption></figcaption>
+						</figure>
+						
+                    	<p></p>
+					</div>
+					<!-- /post content -->
+
+					<!-- post tags -->
+					<div class="section-row">
+						<div class="post-tags">
+							<ul>
+								<li>TAGS:</li>
+								<li><a href="#">Social</a></li>
+								<li><a href="#">Marketing</a></li>
+								<li><a href="#">Digital</a></li>
+								<li><a href="#">Media</a></li>
+							</ul>
+						</div>
+					</div>
+					<!-- /post tags -->
+
+					
+
+					<!-- post author -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">About <a href="author.html">Nandhavardhan Reddy</a></h3>
+						</div>
+						<div class="author media">
+							<div class="media-left">
+								<a href="author.html">
+									<img class="author-img media-object" src="./img/nandhavardhan.jpeg" alt="nandhavardhan_fuerte">
+								</a>
+							</div>
+							<div class="media-body">
+								<p>WEB DEVELOPER</p>
+								<ul class="author-social">
+									<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<!-- /post author -->
+
+					<!-- /related post -->
+					<div>
+						<div class="section-title">
+							<h3 class="title">Related Posts</h3>
+						</div>
+						<div class="row">
+							<!-- post -->
+							<div class="col-md-4">
+								<div class="post post-sm">
+									<a class="post-img" href="marketing-digitalmarketing.php"><img src="./img/BLOGS/digital.png" alt="digital_marketing"></a>
+									<div class="post-body">
+										<div class="post-category">
+											<a href="marketing-digitalmarketing.php">Marketing</a>
+										</div>
+										<h3 class="post-title title-sm"><a href="marketing-digitalmarketing.php">Digital Marketing </a></h3>
+										<ul class="post-meta">
+											<li><a href="author.html">Saksham Sharma</a></li>
+											<li></li>
+										</ul>
+									</div>
+								</div>
+							</div>
+							<!-- /post -->
+
+						
+
+						</div>
+					</div>
+					<!-- /related post -->
+
+					<!-- post comments -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">3 Comments</h3>
+						</div>
+						<div class="post-comments">
+							<!-- comment -->
+							<div class="media">
+								<div class="media-left">
+									<img class="media-object" src="./img/avatar-2.jpg" alt="">
+								</div>
+								<div class="media-body">
+									<div class="media-heading">
+										<h4>John Doe</h4>
+										<span class="time">5 min ago</span>
+									</div>
+									<p>This article was Knowledgeable</p>
+									<a href="#" class="reply">Reply</a>
+									<!-- comment -->
+									<div class="media media-author">
+										<div class="media-left">
+											<img class="media-object" src="./img/avatar-1.jpg" alt="">
+										</div>
+										<div class="media-body">
+											<div class="media-heading">
+												<h4>Jamie Dove</h4>
+												<span class="time">5 min ago</span>
+											</div>
+											<p>This the only way to explore.</p>
+											<a href="#" class="reply">Reply</a>
+										</div>
+									</div>
+									<!-- /comment -->
+								</div>
+							</div>
+							<!-- /comment -->
+
+							
+						</div>
+					</div>
+					<!-- /post comments -->
+
+					<!-- post reply -->
+					<div class="section-row">
+						<div class="section-title">
+							<h3 class="title">Leave a reply</h3>
+						</div>
+						<form class="post-reply">
+							<div class="row">
+								<div class="col-md-12">
+									<div class="form-group">
+										<textarea class="input" name="message" placeholder="Message"></textarea>
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="name" placeholder="Name">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="email" name="email" placeholder="Email">
+									</div>
+								</div>
+								<div class="col-md-4">
+									<div class="form-group">
+										<input class="input" type="text" name="website" placeholder="Website">
+									</div>
+								</div>
+								<div class="col-md-12">
+									<button class="primary-button">Submit</button>
+								</div>
+
+							</div>
+						</form>
+					</div>
+					<!-- /post reply -->
+				</div>
+				<div class="col-md-4">
+					
+
+					<!-- social widget -->
+					<div class="aside-widget">
+                    <div class="section-title">
+                        <h2 class="title">Social Media</h2>
+                    </div>
+                    <div class="social-widget">
+                        <ul>
+                            <li>
+                                <a href="https://www.facebook.com/fuertedevelopers/" class="social-facebook">
+                                <i class="fa fa-facebook"></i>
+                                <span><!--21.2K--><br> Facebook</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="https://twitter.com/fuertedeveloper?lang=en" class="social-twitter">
+                                    <i class="fa fa-twitter"></i>
+                                    <span><!--10.2K--><br>Twitter</span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="#" class="social-google-plus">
+                                    <i class="fa fa-google-plus"></i>
+                                    <span><!--5K--><br>Google</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+              <!-- /social widget -->
+
+					<!-- category widget -->
+					<?php
+					include "cate.php"
+					?>
+					<!-- /category widget -->
+
+					<!-- newsletter widget -->
+					<div class="aside-widget">
+						<div class="section-title">
+							<h2 class="title">Newsletter</h2>
+						</div>
+						<div class="newsletter-widget">
+							<form>
+								<p></p>
+								<input class="input" placeholder="Enter Your Email">
+								<button class="primary-button">Subscribe</button>
+							</form>
+						</div>
+					</div>
+					<!-- /newsletter widget -->
+
+					
+
+					<!-- galery widget -->
+					<?php
+					include "instagram.php"
+					?>
+					<!-- /galery widget -->
+				</div>
+			</div>
+			<!-- /row -->
+		</div>
+		<!-- /container -->
+	</div>
+	<!-- /SECTION -->
+
+</body>
+<?php
+include "footer.php";
+?>
